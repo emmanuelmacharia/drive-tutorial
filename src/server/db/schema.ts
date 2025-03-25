2272308a821b8756f11d42c786f20a59610dad7a
emmanuel macharia <samuelmarsha@outlook.com>
2025-03-25 21:04:55 +0300

    created the schema for the files and folders tables

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,9 +1,25 @@
 
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import { int, text, index, singlestoreTableCreator } from "drizzle-orm/singlestore-core";
 
 
-export const users = singlestoreTable("users_table", {
+export const createTable = singlestoreTableCreator((name) => `drive-tutorial_${name}`);
+
+export const files = createTable("files_table", {
   id: int("id").primaryKey().autoincrement(),
   name: text("name"),
-  age: int("age"),
+  size: text("size"),
+  url: text("url"),
+  parent: int("parent").notNull(),
+  // type: text("type"),
+}, (t) => {
+  return [index("parent_index").on(t.parent)];
+});
+
+
+export const folder = createTable("folders_table", {
+  id: int("id").primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: int("parent"),
+}, (t) => {
+  return [index("parent_index").on(t.parent)];
 });
