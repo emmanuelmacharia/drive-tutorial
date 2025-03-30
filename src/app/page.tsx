352dbc78647e681a82fe8f47169aee6ac54bf9f7
emmanuel macharia <samuelmarsha@outlook.com>
2025-03-30 13:56:57 +0300

    purged everything from the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,5 @@ export default function GoogleDriveClone() {
   // const files = await db.select().from(fileSchema);
   // const folders = await db.select().from(foldersSchema);
   // return <DriveContents files={files} folders={folders} />;
-  <div className="">Please subscribe</div>
+  return <div className="">Hello World</div>
 }
