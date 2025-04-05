1137497f3e318bf90b9f3577f9a6d458784be9c2
emmanuel macharia <samuelmarsha@outlook.com>
2025-04-05 15:11:28 +0300

    access control message

--- src/app/folder/[folderId]/page.tsx
+++ src/app/folder/[folderId]/page.tsx
@@ -39,6 +39,11 @@ export default async function GoogleDriveClone(props: {
     return <div>{rootFolder.error}</div>;
   }
 
+  const currentFolder = folders.find((folder) => folder.id === parsedFolderId);
+  if (currentFolder?.ownerId !== session.userId) {
+    return <div>{`You do not have access to this folder`}</div>; // should not have access to the folder
+  }
+
   return (
     <DriveContents
       files={files}
